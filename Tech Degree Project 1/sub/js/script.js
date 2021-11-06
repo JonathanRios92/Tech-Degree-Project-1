ac01a8c0f3ab00b3e4fdb5748be6fee06b7e3a04
JonathanRios92 <jonathan.rios1123@gmail.com>
2021-11-05 22:38:38 -0400

    work on getting the button to change the quote

--- Tech Degree Project 1/sub/js/script.js
+++ Tech Degree Project 1/sub/js/script.js
@@ -19,44 +19,75 @@ let quotes = [
     year: '2017'
   },
   {
-    quote: '',
-    source: '',
+    quote: 'A journey of a thousand miles begins with a single step.',
+    source: 'Chinese Proverb',
+    citation: 'wow4u',
+    year: '1875'
+  },
+  {
+    quote: 'The way to get started is to quit talking and begin doing.',
+    source: 'Walt Disney',
     citation: '',
+    year: '1950'
+  },
+  {
+    quote: 'The easily outraged are easily manipulated. If you can’t control your emotions, other people will',
+    source: 'Gurwinder',
+    citation: 'Reddit',
     year: ''
   },
   {
-    quote: '',
-    source: '',
-    citation: '',
-    year: ''
-  },
-  {
-    quote: '',
-    source: '',
-    citation: '',
-    year: ''
-  },
-  {
-    quote: '',
-    source: '',
-    citation: '',
-    year: ''
+    quote: 'Age is just a number, and your talent will never fail you. It has no expiry date.',
+    source: 'Madhuri Dixit',
+    citation: 'BrainyQuote',
+    year: '2001'
   }
-
+  
 ];
 
-console.log(quotes);
+
 
 
 /***
  * `getRandomQuote` function
 ***/
 
+function getRandomQuote() {
+  let randomNumber = quotes[Math.floor(Math.random() * quotes.length)];
+  return randomNumber;
+};
+
 
 
 /***
  * `printQuote` function
 ***/
+
+function printQuote() {
+  let randomQuote = getRandomQuote();
+  
+  let html = `
+  <p class="quote">${randomQuote.quote}</p>
+  <p class="source">${randomQuote.source} 
+  `
+  
+  if ( randomQuote.citation == '' ) {
+    
+  } else {
+    html += `<span class="citation">${randomQuote.citation}</span>`;
+  };
+
+    if ( randomQuote.year == '' ) {
+    
+  } else {
+    html += `<span class="citation">${randomQuote.year}</span>
+    </p>
+    `
+  }
+  
+  return html;
+ 
+};
 
 
 
@@ -65,4 +96,7 @@ console.log(quotes);
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
+
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+
+document.getElementById('quote-box').innerHTML = printQuote(); 
